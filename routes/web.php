cf547788338f75526c6ba1c1b9d266a80248509c
shafa <132644198+shafakirrn@users.noreply.github.com>
2024-09-12 15:14:47 +0700

    section 2 (vid 2-5)

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Shafa Kirana', 'title' => 'About' ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
