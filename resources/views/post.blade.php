9b4bf32a80ac6a247f2f101cd4b3c842220e1a89
shafa <132644198+shafakirrn@users.noreply.github.com>
2024-09-30 16:27:51 +0700

    update section 4 (video 10-11)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,12 +5,12 @@
     <article class="py-8 max-w-screen-md ">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post ['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+            <a href="#">{{ $post->author->name}}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class ="my-4 font-light">
             {{ $post ['body'] }}
         </p>
-        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
+        <a href="/authors/{{ $post->author->id }}" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
     </article>
    
 </x-layout>
